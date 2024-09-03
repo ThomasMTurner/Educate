eb759be21cfd0c072de4b3f5cf2693eb4eeec1f7
Thomas Turner <thomasturner090205@gmail.com>
2024-09-03 18:52:10 +0100

    Completed meta search request building

--- searchinterface/src/config_utilities.jsx
+++ searchinterface/src/config_utilities.jsx
@@ -4,10 +4,7 @@ import { useAuth } from '.../../AuthProvider';
 
 export const writeConfig = async (config) => {
     try {
-        // const response =
         await axios.post("http://localhost:9797/config/write", config);
-        // Previously had redundant call to response.data, my write request currently
-        // does not have a return body.
     } catch (error) {
         console.error(error);
         throw new Error('Failed to write new config: ', error);
--- src/meta.rs
+++ src/meta.rs
@@ -1,19 +1,15 @@
-use std::collections::HashMap;
-use std::sync::Arc;
-use tokio::sync::Mutex;
 use std::error::Error;
 use futures::stream::{self, StreamExt};
-use std::env;
 use serde::{Deserialize, Serialize};
 use crate::services::DocumentResult;
 use rocket::serde::json::Json;
 use std::process::Command;
+use regex::Regex;
 //use geolocation;
 
 // NOTE: thiserror is often recommended for error type building.
-// NOTE: probably should use issue tracking rather 
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum SearchResponse {
     Search(DocumentResult),
     MetaSearch(MetaSearchResult)
@@ -27,12 +23,14 @@ pub enum SearchResult {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MetaSearchResult {
-    anon: String,
+    pub title: String,
+    pub url: String,
+    pub description: String
 }
 
 impl MetaSearchResult {
-    fn new(anon: String) -> Self {
-        MetaSearchResult { anon }
+    fn new(title: String, url: String, description: String) -> Self {
+        MetaSearchResult { title, url, description }
     }
 }
 
@@ -50,75 +48,64 @@ impl MetaSearchRequest {
         MetaSearchRequest { browser, /*location,*/ q }
     }
 
-    async fn collect(&self) -> Result<SearchResponse, ()> {
+    async fn collect(&self) -> Result<Vec<SearchResponse>, ()> {
        match self.browser.as_str() {
             "DuckDuckGo" => {
                 println!("Collecting DuckDuckGo results");
-                Ok(self.ddgr_collect().await)
-
+                match self.ddgr_collect().await {
+                    Ok(response) => Ok(response),
+                    Err(e) => panic!("Need to implement error handling here: {}", e)
+                }
             }
+            /*
             "Google" => {
-                Ok(self.googler_collect().await)
+                //
             }
+            */
             _ => {
                 eprintln!("Unknown browser: {}", self.browser);
                 Err(())
             }
         } 
     }
-
-
-    /// TO DO (LATER):
-    /// BOTH OF THESE CAN BE MODIFIED TO INCORPORATE USER LOCALE AND 
-    /// FURTHER SEARCH FILTERS.
-    /// I.E. LOCALE INFORMATION CAN BE USED (AREA CODE) WITH -R 
-    /// PARAMETER WITH DDGR TO GET RESULTS FOR A SPECIFIC COUNTRY.
-    /// ALSO NEED TO SET PROPER ERROR HANDLING.
     
     // TO DO:
-    // Does not require API key - simply need to build command for simple
-    // search (currently we do not have search filters).
-    async fn ddgr_collect(&self) -> SearchResponse {
+    // Define a proper regular expression for capture groups ((number . )(some text)([url])(some
+    // text))
+    // Test this works for ddgr and googler outputs.
+    fn process_with_expression(&self, raw: &str) -> Result<Vec<SearchResponse>, regex::Error> {
+        // Define regex for simple pattern (number.)(title)([url])(description).
+        let re = Regex::new(r"(\d+)\.\s*(.+)\s*(\[.+?\])\s*(.+)")?;
+        
+        // Convert captures to MetaSearchResult.
+        let captures: Vec<SearchResponse> = re.captures_iter(raw)
+            .map(|cap| SearchResponse::MetaSearch(
+                MetaSearchResult::new(cap[2].to_string(), cap[3].to_string(), cap[4].to_string())))
+            .collect();
+
+        Ok(captures)
+    }
+    
+
+    async fn ddgr_collect(&self) -> Result<Vec<SearchResponse>, regex::Error> {
         let output = Command::new("ddgr")
             .args(self.q.split_whitespace())
             .output()
             .expect("Failed to execute ddgr");
 
-        println!("DDGR output: {}", String::from_utf8_lossy(&output.stdout));
-        
-        SearchResponse::MetaSearch(MetaSearchResult::new(
-            String::from_utf8_lossy(&output.stdout).to_string()))
-    }
-    
-    // TO DO:
-    // Does require API key - need to see how to collect this appropriately.
-    async fn googler_collect(&self) -> SearchResponse {
-        let key = self.browser.to_uppercase() + "_API_KEY";
-        if let Ok(v) = env::var(key) {
-            // Set API key for Googler config.
-            println!("API key for Googler: {}", v);
-            
-        } else {
-            eprintln!("Could not find API key for Googler")
-        }
-
-       
-        // Build output.
-        
-
-        SearchResponse::MetaSearch(MetaSearchResult::new(
-            String::from(" ")
-        ))
-
+        let raw = String::from_utf8_lossy(&output.stdout);
+        //println!("Raw: {}", raw);
+        let results = self.process_with_expression(&raw)?;
+        println!("Results: {:?}", results);
+        Ok(results)
     }
     
     // May want to extract more useful information from the engine directly
     // using some available Search API as opposed to simple command line tools.
+    // This will be preferable due to issues with Googler (mostly deprecated).
     
 }
 
-// CURRENTLY: we removed instantiation of Tokio runtime as this 
-// is already done by Rocket.
 pub async fn aggregate(requests: Vec<MetaSearchRequest>) -> Result<Vec<SearchResponse>, Box<dyn Error>> {
     let mut responses: Vec<SearchResponse> = Vec::with_capacity(requests.len());
     let concurrent = requests.len();
@@ -134,7 +121,7 @@ pub async fn aggregate(requests: Vec<MetaSearchRequest>) -> Result<Vec<SearchRes
 
     for result in results {
         match result {
-            Ok(res) => responses.push(res),
+            Ok(res) => responses.extend(res),
             Err(_) => eprintln!("Error obtaining meta search response"),
         }
     }
